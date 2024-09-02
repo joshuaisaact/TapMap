1e8fe7e40c41a106d0f518702be7f3724296e518
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-09-02 16:45:14 +0100

    refactor: OOP using an app class

--- src/script.js
+++ src/script.js
@@ -11,62 +11,82 @@ const inputFood = document.querySelector('.form_input--food');
 const inputPets = document.querySelector('.form_input--pets');
 const inputOutdoors = document.querySelector('.form_input--outdoors');
 
-let map, mapEvent;
+// let map, mapEvent;
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(function (position) {
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+
+    form.addEventListener('submit', this._newTap.bind(this));
+
+    inputType.addEventListener('change', this._toggleOutdoor);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your location');
+        },
+      );
+  }
+
+  _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
 
-    const coords = [latitude, longitude]
+    const coords = [latitude, longitude];
 
-    map = L.map('map').setView(coords, 14);
+    this.#map = L.map('map').setView(coords, 14);
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
+    this.#map.on('click', this._showForm.bind(this));
+  }
 
+  _showForm(mapEvent) {
+    this.#mapEvent = mapEvent;
+    form.classList.remove('hidden');
+    inputFood.focus();
+  }
 
-    map.on('click', function (mapE) {
-      mapEvent = mapE;
-      form.classList.remove('hidden');
-      inputFood.focus();
-      // console.log(mapEvent);
-      // const { lat, lng } = mapEvent.latlng;
+  _toggleOutdoor() {
+    inputPets.closest('.form__row').classList.toggle('form__row--hidden');
+    inputOutdoors.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-      // L.marker([lat, lng])
-      //   .addTo(map)
-      //   .bindPopup(L.popup({ maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: "custom-popup" }))
-      //   .setPopupContent('Pub')
-      //   .openPopup();
-    })
-  },
-    function () {
-      alert('Could not get your location')
-    })
+  _newTap(e) {
+    e.preventDefault();
 
+    // Clear input fields
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
+    inputPets.value = ' ';
+    inputFood.value = ' ';
+    inputOutdoors.value = ' ';
 
-  // Clear input fields
+    // Display Marker
+    const { lat, lng } = this.#mapEvent.latlng;
 
-  inputPets.value = ' ';
-  inputFood.value = ' ';
-  inputOutdoors.value = ' ';
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'custom-popup',
+        }),
+      )
+      .setPopupContent('Pub')
+      .openPopup();
+  }
+}
 
-  // Display Marker
-  const { lat, lng } = mapEvent.latlng;
-
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(L.popup({ maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: "custom-popup" }))
-    .setPopupContent('Pub')
-    .openPopup();
-})
-
-inputType.addEventListener('change', function () {
-  inputPets.closest('.form__row').classList.toggle('form__row--hidden');
-  inputOutdoors.closest('.form__row').classList.toggle('form__row--hidden');
-})
+const app = new App();
