37d5e971c5ef263e455482369ad5ecf8082ef643
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-09-02 15:55:35 +0100

    feat: submit pub, styling

--- src/script.js
+++ src/script.js
@@ -6,10 +6,12 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 const form = document.querySelector('.form');
 const containerPins = document.querySelector('.pins');
 const inputType = document.querySelector('.form__input--type');
-const inputPrice = document.querySelector('.form__input--price');
-const inputFood = document.querySelector('.form__input--food');
-const inputPets = document.querySelector('.form__input--pets');
-const inputOutdoors = document.querySelector('.form__input--outdoors');
+const inputPrice = document.querySelector('.form_input--price');
+const inputFood = document.querySelector('.form_input--food');
+const inputPets = document.querySelector('.form_input--pets');
+const inputOutdoors = document.querySelector('.form_input--outdoors');
+
+let map, mapEvent;
 
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(function (position) {
@@ -18,15 +20,48 @@ if (navigator.geolocation)
 
     const coords = [latitude, longitude]
 
-    const map = L.map('map').setView(coords, 14);
+    map = L.map('map').setView(coords, 14);
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      .openPopup();
-  }, function () {
-    alert('Could not get your location')
-  })
+
+
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputFood.focus();
+      // console.log(mapEvent);
+      // const { lat, lng } = mapEvent.latlng;
+
+      // L.marker([lat, lng])
+      //   .addTo(map)
+      //   .bindPopup(L.popup({ maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: "custom-popup" }))
+      //   .setPopupContent('Pub')
+      //   .openPopup();
+    })
+  },
+    function () {
+      alert('Could not get your location')
+    })
+
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+
+  inputPets.value = ' ';
+  inputFood.value = ' ';
+  inputOutdoors.value = ' ';
+
+  // Display Marker
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(L.popup({ maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: "custom-popup" }))
+    .setPopupContent('Pub')
+    .openPopup();
+})
